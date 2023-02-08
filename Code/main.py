824a2953885a92c5d186df8bd2de49a63cc90ee8
Max Maiche <max.maiche@gmail.com>
2023-02-08 12:09:33 +0100

    Images and 0 -> 24 Label

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,10 @@
-#C'est le main salut
+import numpy as np
+import skimage as ski
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+image = mpimg.imread('Ressources/0.jpg').copy()
+
+plt.imshow(image)
+plt.show()
