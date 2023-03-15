d82f06b8a82f7f1eacb0b3551a8bccc98f1b05b7
Luc Salvon <salvon.luc@gmail.com>
2023-03-15 11:09:33 +0100

    On s'en fout on s'en fout

--- Code/main.py
+++ Code/main.py
@@ -3,25 +3,33 @@ import numpy as np
 import skimage as ski
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import cv2
 
 nomImage = "../Ressources/Images/" + input("Nom de l'image : ")
 image = mpimg.imread(nomImage).copy()
-print(image)
+# print(image)
 
-formatImage = nomImage.split('.')[-1]
+# formatImage = nomImage.split('.')[-1]
 
-if formatImage == "png":
+# if formatImage == "png":
+if image[0, 0, 0] is float and image[0, 0, 1] is float:
     image = np.uint8(255 * image)
 
-imageGrayscale = [[round(sum(image[i, j]) / 3) for j in range(image.shape[1])] for i in range(image.shape[0])]
+print(image)
 
-sobel = [[-1, -2, -1],
-         [0, 0, 0],
-         [1, 2, 1]]
+imageGrayscale = np.array([[round(sum(image[i, j]) / 3) for j in range(image.shape[1])] for i in range(image.shape[0])])
 
-imageConvolution = scipy.signal.fftconvolve(imageGrayscale, sobel)
+sobel = np.array([[-1, -2, -1],
+                  [0, 0, 0],
+                  [1, 2, 1]])
 
-#jpeg : plt.imread(image,format="jpeg")images = []
+imageConvolution = scipy.signal.convolve2d(imageGrayscale, sobel, mode='same', boundary='fill', fillvalue=0)
+# imageConvolution = np.array([[int(imageConvolution[i, j]) for j in range(imageConvolution.shape[1])] for i in
+#                              range(imageConvolution.shape[0])])
+
+print(imageConvolution)
+
+# jpeg : plt.imread(image,format="jpeg")images = []
 # for i in range(100):
 #     try:
 #         image = mpimg.imread('Ressources/' + str(i) + '.jpg').copy()
@@ -30,8 +38,11 @@ imageConvolution = scipy.signal.fftconvolve(imageGrayscale, sobel)
 #     except:
 #         pass
 
+imageConvolution = np.array([[abs(imageConvolution[i, j]) for j in range(imageConvolution.shape[1])] for i in range(imageConvolution.shape[0])])
+
+
 plt.imshow(imageGrayscale, cmap='gray', vmin=0, vmax=255)
 plt.show()
 
-plt.imshow(imageConvolution, cmap='gray')
+plt.imshow(imageConvolution, vmin=0, vmax=255)
 plt.show()
