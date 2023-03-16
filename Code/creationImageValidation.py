56e47f88c19f06c86ef5904de43c4d488c27d58e
Max Maiche <max.maiche@gmail.com>
2023-03-16 02:47:32 +0100

    je me suis amusé
    je garanti rien mais c'est stylé

--- Code/creationImageValidation.py
+++ Code/creationImageValidation.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import json
 from PIL import Image
-
+from time import time
+from random import shuffle, randint
 def getLabeling(nomImage: str):
-    #f = open('../Ressources/labeling/' + nomImage + '.json')
-    f = open('../Ressources/testsimple/petittest.json')
+    f = open('../Ressources/labeling/' + nomImage + '.json')
+    #f = open('../Ressources/testsimple/petittest.json')
     data = json.load(f)
     shapes = data['shapes']
 
@@ -59,7 +60,34 @@ def getpixelSegment(point1, point2):
     return pixelinSegment
 
 
+def getNeighbours(image, point):
+    neighbours = []
+    x=point[0]
+    y=point[1]
 
+    try:
+        if image[x-1, y] == 0:
+            neighbours.append((x-1, y))
+    except:
+        pass
+    try:
+        if image[x+1, y] == 0:
+            neighbours.append((x+1, y))
+    except:
+        pass
+    try:
+        if image[x, y-1] == 0:
+            neighbours.append((x, y-1))
+    except:
+        pass
+    try:
+        if image[x, y+1] == 0:
+            neighbours.append((x, y+1))
+    except:
+        pass
+
+    #shuffle(neighbours)
+    return neighbours
 
 
 def pixelinpolygones(height, width, polygone):
@@ -92,37 +120,122 @@ def pixelinpolygones(height, width, polygone):
     for p in contours:
         image[p[1], p[0]] = 255
 
-    for x in range(max_y-1, min_y-1, -1):
-        for y in range(min_x, max_x):
-            if image[x, y] == 0:
-                image[x, y] = castRay(image, (x, y), max_x)
+    #mean of the contour
+    mean_x = (max_x+min_x)/2
+    mean_y = (max_y+min_y)/2
+    print(mean_x, mean_y)
 
+    pixelQueue = [(int(mean_y), int(mean_x), True, True)]
+    cpt = 0
 
+    try:
+        while len(pixelQueue) > 0:
+            cpt += 1
+            #if cpt % 1000 == 0:
+                #print(len(pixelQueue), 'pixels left')
+
+            x, y, hory, verti = pixelQueue.pop(0)
+            pixel = (x, y)
+            if hory:
+                pixelQueue.append(castRayTop(image, pixel))
+                pixelQueue.append(castRayBottom(image, pixel))
+            if verti:
+                pixelQueue.append(castRayLeft(image, pixel))
+                pixelQueue.append(castRayRight(image, pixel))
+
+    except KeyboardInterrupt:
+        pass
+
+    # try:
+    #     for x in range(max_y-1, min_y-1, -1):
+    #         print(((x-min_y)/(max_y-min_y)*100), '%')
+    #         for y in range(min_x, max_x):
+    #             if image[x, y] == 0:
+    #                 image[x, y] = castRay(image, (x, y), max_x)
+    #
+    # except KeyboardInterrupt:
+    #     pass
+    #
+    # for x in range(min_x, max_x):
+    #     for y in range(min_x, max_x):
+    #         if image[x, y] == 128:
+    #             image[x, y] = 255
     return image
 
+def castRayTop(image, point):
+    x, y = point
+    x_init = x
 
+    try:
+        while image[x-1, y] != 255:
+            x -= 1
+            image[x, y] = 128
+    except:
+        pass
+    x += (x_init-x) // 2
+    return x, y, False, True
+
+def castRayBottom(image, point):
+    x, y = point
+    x_init = x
+    try:
+        while image[x+1, y] != 255:
+            x += 1
+            image[x, y] = 128
+    except:
+        pass
+    x +=( x_init-x) // 2
+    return x, y, False, True
+
+def castRayLeft(image, point):
+    x, y = point
+    y_init = y
+    try:
+        while image[x, y-1] != 255:
+            y -= 1
+            image[x, y] = 128
+    except:
+        pass
+    y += (y_init-y) // 2
+    return x, y, True, False
+
+def castRayRight(image, point):
+    x, y = point
+    y_init = y
+    try:
+        while image[x, y+1] != 255:
+            y += 1
+            image[x, y] = 128
+    except:
+        pass
+    y += (y_init-y) // 2
+    return x, y, True, False
 def castRay(image, point, max_x):
 
     nbInter = 0
     x, y = point
-    x_init = x
-
     while x < max_x:
         if image[x, y] == 255:
             nbInter += 1
-            while image[x, y] == 255:
+            while image[x, y] == 255 and x < max_x:
                 x += 1
+
+        if x < max_x-1 and image[x+1, y] == 128:
+            return 128
 
         x += 1
 
     if nbInter % 2 == 1:
-        return 254
+        return 128
     else:
         return 0
 
 
 height, width, b, l, s = getLabeling('33')
-image = pixelinpolygones(height, width, b[0])
+timeStart = time()
+image = pixelinpolygones(height, width, b[1])
+timeEnd = time()
+print('time: ', timeEnd - timeStart)
 im = Image.fromarray(image)
 im = im.convert('RGB')
 im.save("resultat_Validation.png")
