0c51e89cece47f5ceed06eeaf08877b0637412d5
Max Maiche <max.maiche@gmail.com>
2023-03-21 17:44:26 +0100

    Avancement

--- Code/mainbis.py
+++ Code/mainbis.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-
+import math
 # Import image
 ok = False
 while not ok:
@@ -10,36 +10,87 @@ while not ok:
     except:
         pass
 
+
+# Resize image
+width = img.shape[1]
+height = img.shape[0]
+ratio = 1024 / width
+dim = (1024, int(height * ratio))
+
+img = cv.resize(img, dim)
+
 # Make image grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # Do edge detection
 edges = cv.Canny(gray, 50, 150, apertureSize=3)
 
+# Ouverture
+#kernel = np.ones((3, 3), np.uint8)
+#cv.morphologyEx(edges, cv.MORPH_OPEN, kernel)
+#edges = cv.erode(edges, kernel, iterations=1)
+#edges = cv.dilate(edges, kernel, iterations=1)
+
 # Dilate edge detection image so the lines are more readable
-kernel = np.ones((11, 5), np.uint8)
+kernel = np.ones((3, 3), np.uint8)
 edges = cv.dilate(edges, kernel, iterations=1)
 
-# Identify lines
-lines = cv.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=400, maxLineGap=30)
+# Closure
+kernel = np.ones((7, 7), np.uint8)
+edges = cv.dilate(edges, kernel, iterations=1)
+edges = cv.erode(edges, kernel, iterations=1)
+#cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel)
 
-maxLine1 = (0, 0, 0, 0)
-maxLine2 = (0, 0, 0, 0)
+# Identify lines
+lines = cv.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=5)
+
+lineTop = (0, 0, img.shape[0], 0)
+lineBottom = (0, img.shape[1], img.shape[0], img.shape[1])
+
+if lines is None:
+    print(":/")
+    exit()
 
 # Display each line on the original image
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    if abs(x1 - x2) > abs(maxLine1[0] - maxLine1[2]):
-        maxLine1 = line[0]
-    elif abs(y1 - maxLine1[1]) > 100 and abs(x1 - x2) > abs(maxLine2[0] - maxLine2[2]):
-        maxLine2 = line[0]
+    if (y1 + y2) / 2 > img.shape[0] / 2:
+        if (y1 + y2) / 2 > lineTop[1] and abs(x1 - x2) > 700:
+            if y1 - lineTop[1] > 50 or abs(lineTop[0] - lineTop[2]) < abs(x1 - x2):
+                lineTop = (x1, y1, x2, y2)
+    else:
+        if (y1 + y2) / 2 < lineBottom[1] and abs(x1 - x2) > 700:
+            if lineBottom[1] - y1 > 50 or abs(lineTop[0] - lineTop[2]) < abs(x1 - x2):
+                lineBottom = (x1, y1, x2, y2)
 
-    cv.line(img, (x1, y1), (x2, y2), (0, 200, 200), 2)
+    # cv.line(img, (x1, y1), (x2, y2), (0, 200, 200), 2)
 
-cv.line(img, (maxLine1[0], maxLine1[1]), (maxLine1[2], maxLine1[3]), (0, 0, 255), 10)
-cv.line(img, (maxLine2[0], maxLine2[1]), (maxLine2[2], maxLine2[3]), (0, 0, 255), 10)
+
+if lineTop[1] > lineBottom[1]:
+    lineTop, lineBottom = lineBottom, lineTop
+
+cv.line(img, (lineTop[0], lineTop[1]), (lineTop[2], lineTop[3]), (0, 0, 255), 10)
+cv.line(img, (lineBottom[0], lineBottom[1]), (lineBottom[2], lineBottom[3]), (0, 0, 255), 10)
+
+pts1 = np.float32([[lineTop[0], lineTop[1]], [lineTop[2], lineTop[3]], [lineBottom[2], lineBottom[3]], [lineBottom[0], lineBottom[1]]])
+ratio = ((abs(lineTop[0] - lineTop[2]) + abs(lineBottom[0] - lineBottom[2])) / 2) / ((abs(lineTop[1] - lineBottom[1]) + abs(lineTop[3] - lineBottom[3])) / 2)
+print("Ligne1 : ", lineTop)
+print("Ligne2 : ", lineBottom)
+print(ratio)
+
+boardW = 1024
+boardH = int(boardW/ratio)
+
+pts2 = np.float32([[0, 0], [boardW, 0], [boardW, boardH], [0, boardH]])
+
+M = cv.getPerspectiveTransform(pts1, pts2)
+
+transformed = np.zeros((int(boardW), int(boardH)), dtype=np.uint8)
+dst = cv.warpPerspective(img, M, transformed.shape)
+
 
 # Save images
 cv.imwrite('gray.jpg', gray)
 cv.imwrite('edges.jpg', edges)
 cv.imwrite('resultat.jpg', img)
+cv.imwrite('transformed.jpg', dst)
--- Code/validation.py
+++ Code/validation.py
@@ -2,14 +2,31 @@ import numpy as np
 import skimage as ski
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import cv2 as cv
 
-def getNbPixelBlanc(imgBin):
-    nbPixelB = 0
-    for i in range(0, imgBin.shape[0]):
-        for j in range(0, imgBin.shape[1]):
-            if imgBin[i, j] == 1:
-                nbPixelB += 1
-    return nbPixelB
 
-# fait un truc pour faire l'union des deux et l'inter des deux images binaires
-# entre json et le png/jpg ?
+def compare(image1, image2):
+
+    #resize
+    width1 = image1.shape[1]
+    width2 = image2.shape[1]
+
+    if width1 > width2:
+        ratio = width2 / width1
+        dim = (width2, int(image1.shape[0] * ratio))
+        image1 = cv.resize(image1, dim)
+    else:
+        ratio = width1 / width2
+        dim = (width1, int(image2.shape[0] * ratio))
+        image2 = cv.resize(image2, dim)
+
+    sumUnion = 0
+    sumInter = 0
+    for i in range(0, image1.shape[0]):
+        for j in range(0, image1.shape[1]):
+            if image1[i, j] != 0 or image2[i, j] != 0:
+                sumUnion += 1
+            if image1[i, j] != 0 and image2[i, j] != 0:
+                sumInter += 1
+
+    return sumInter / sumUnion
